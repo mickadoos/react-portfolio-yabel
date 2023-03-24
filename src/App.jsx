4e1328df7a1968831459b815566bc000abd73ce2
Yabel <yabelrr@gmail.com>
2023-03-24 16:03:08 +0100

    New portfolio web started

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Header from './components/header/Header.jsx';
+import Nav from './components/nav/Nav.jsx';
+import About from './components/about/About.jsx';
+import Skills from './components/skills/Skills.jsx';
+import Portfolio from './components/portfolio/Portfolio.jsx';
+import Contact from './components/contact/Contact.jsx';
+import Footer from './components/footer/Footer.jsx';
+
+const App = () => {
+  return (
+    <div>
+        <Header></Header>
+        <Nav></Nav>
+        <About></About>
+        <Skills></Skills>
+        <Portfolio></Portfolio>
+        <Contact></Contact>
+        <Footer></Footer>
+    </div>
+  )
+}
+
+export default App
