1dae4bb571652f84257c55b0828de15cfe590532
Yabel <yabelrr@gmail.com>
2023-03-28 13:29:09 +0200

    Contact Component, DONE. Watch word-break for mail

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
+// 15.0 copy paste the contact component, install emailjs-com (look tut)
+// 3.0 create all components folders with .jsx file and .css --- then on .jsx file type racfe, press tab and import the .css file 
+import "./Contact.css";
+// import "./Contact2.css";
+import { MdOutlineMail } from "react-icons/md";
+import {BsWhatsapp} from 'react-icons/bs'
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
-  return (
-    <section id='contact'>
-    Contact
-    </section>
-  )
-}
+  const form = useRef();
 
-export default Contact
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_ih2dd0g', 'template_995n86b', form.current, 'KGoMH25doopl9-LCW')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+    e.target.reset();
+  };
+
+  return (
+    // 10.2 chanmge all components to a section parent container with his correspondent id
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineMail  className="contact__option-icon"/>
+            <h4>Email</h4>
+            <h5>yabelrr@gmail.com</h5>
+            <a href="mailto:yabelrr@gmail.com" target="_blank" rel="noreferrer">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className="contact__option-icon"/>
+            <h4>WhatsApp</h4>
+            <h5>+34 687279232</h5>
+            <a href="https://wa.me/34687279232" target="_blank" rel="noreferrer">Send a message</a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Your Full Name" required />
+          <input type="email" name="email" placeholder="Your email" required />
+          <textarea name="message" rows="7" placeholder="Your Message" required></textarea>
+          <button type="submit" className="btn btn-primary">Send Message</button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
