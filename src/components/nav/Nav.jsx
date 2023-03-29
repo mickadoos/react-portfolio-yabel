c6e3de8334b9e2a93c5a898f07123a9dfb0bf63c
Yabel <yabelrr@gmail.com>
2023-03-29 09:43:58 +0200

    Fullstack title color changed, navbar color changed, footer socials and copyright ready

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import './Nav.css'
 import {AiOutlineUser} from 'react-icons/ai'
 import {BiBook} from 'react-icons/bi'
-import {RiServiceLine} from 'react-icons/ri'
 import {BiMessageSquareDetail} from 'react-icons/bi'
+import {TbTools} from 'react-icons/tb'
 import { useState } from 'react' 
 
 
@@ -13,8 +13,8 @@ const Nav = () => {
     <div>
     <nav>
       <a href='# ' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
-      <a href='#portfolio' onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><RiServiceLine/></a>
-      <a href='#skills' onClick={() => setActiveNav('#skills')} className={activeNav === '#skills' ? 'active' : ''}><BiBook/></a>
+      <a href='#portfolio' onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><BiBook/></a>
+      <a href='#skills' onClick={() => setActiveNav('#skills')} className={activeNav === '#skills' ? 'active' : ''}><TbTools/></a>
       <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
     </nav>
     </div>
