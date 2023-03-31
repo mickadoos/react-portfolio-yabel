9f2f788d3a20a426db9dcb839ba022525a9f087e
Yabel <yabelrr@gmail.com>
2023-03-31 10:29:28 +0200

    About component update: remove cards component, changed fullstack color variable, added margin to about__content, fixed image profile responsiveness

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,24 +2,24 @@ import React from "react";
 import "./About.css";
 import ME from "../../assets/foto-yabel.JPG";
 import CTA from "./CTA.jsx";
-import DPLANIMG from "../../assets/dplan-project-image.png";
-import GameProjectIMG from "../../assets/game-project-x-image.png";
+// import DPLANIMG from "../../assets/dplan-project-image.png";
+// import GameProjectIMG from "../../assets/game-project-x-image.png";
 
 const About = () => {
-  const data = [
-    {
-      title: "DPLAN",
-      image: DPLANIMG,
-      subtitle: "Full Stack App",
-      demo: "https://famous-brioche-240d75.netlify.app/",
-    },
-    {
-      title: "Game Project",
-      image: GameProjectIMG,
-      subtitle: "Frontend App",
-      demo: "https://mickadoos.github.io/game-project-x/",
-    },
-  ];
+  // const data = [
+  //   {
+  //     title: "DPLAN",
+  //     image: DPLANIMG,
+  //     subtitle: "Full Stack App",
+  //     demo: "https://famous-brioche-240d75.netlify.app/",
+  //   },
+  //   {
+  //     title: "Game Project",
+  //     image: GameProjectIMG,
+  //     subtitle: "Frontend App",
+  //     demo: "https://mickadoos.github.io/game-project-x/",
+  //   },
+  // ];
   return (
     <section id="about">
       <div className="container about__container">
@@ -40,7 +40,7 @@ const About = () => {
           <h2 id="full-stack" data-text="Full Stack Developer">
             Full Stack Developer
           </h2>
-          <div className="about__cards">
+          {/* <div className="about__cards">
             {data.map(({ title, image, subtitle, demo }) => {
               return (
                 <a href={demo} target="_blank" rel="noreferrer">
@@ -56,7 +56,7 @@ const About = () => {
                 </a>
               );
             })}
-          </div>
+          </div> */}
 
           <p>
             I am a Full-Stack developer who graduated from Ironhack and
